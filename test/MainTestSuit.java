93440767d68c50ac4419f436d6b161867c013f87
Stepanenko Anatoliy <tolik.stepanenko@gmail.com>
2015-05-20 15:39:59 +0300

    init commit

---
+++ test/MainTestSuit.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.Logger;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+
+/**
+ * Quitok.
+ * Created by Anatoliy on 20.05.2015.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({})
+public class MainTestSuit {
+    private static final Logger log = Logger.getLogger(MainTestSuit.class);
+
+
+}
